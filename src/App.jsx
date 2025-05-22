4757c3797a50eee2b9849d914ad2d2f1cb663722
Fikre-M <fikreddu@gmail.com>
2025-05-22 19:23:45 -0400

    lets finish

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+
+import './App.css'
+import Home from './Home/Home'
+
+function App() {
+
+  return (
+    <>
+      <>
+        <Home />
+        
+      </>
+      
+    </>
+  )
+}
+
+export default App
